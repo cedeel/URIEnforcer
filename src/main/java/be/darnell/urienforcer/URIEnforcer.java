3318567b7b300d737d9d8d4bc61d2210cdca6301
cedeel <chris@cedeel.com>
2013-02-17 21:23:35 +0100

    Added support for several IPs.

--- src/main/java/be/darnell/urienforcer/URIEnforcer.java
+++ src/main/java/be/darnell/urienforcer/URIEnforcer.java
@@ -27,42 +27,54 @@
 package be.darnell.urienforcer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import java.util.List;
+import org.bukkit.ChatColor;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.MetricsLite;
 
 public class URIEnforcer extends JavaPlugin implements Listener {
-    
+
     private static final int DEFAULT_PORT = 25565;
-    private String ip;
-    
+    private List<String> addresses;
+    private List<String> names;
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        
         saveDefaultConfig();
         FileConfiguration config = getConfig();
-        ip = config.getString("ip", "localhost");
+        if (!config.isList("ip"))
+            addresses = new ArrayList<String>(Arrays.asList(config.getString("ip", "localhost")));
+        else {
+            addresses = config.getStringList("ip");
+        }
+        names = new ArrayList<String>();
+        for (String name : addresses) {
+            names.add((name + ":" + getServer().getPort()).toLowerCase());
+        }
         if (config.getBoolean("mcstats", true))
             setupMetrics();
     }
-    
+
     @EventHandler
     public void onLogin(PlayerLoginEvent e) {
-        String desiredname = ip + ":" + getServer().getPort();
-        if (!e.getHostname().equalsIgnoreCase(desiredname)) {
+
+        if (!names.contains(e.getHostname().toLowerCase())) {
             if (getServer().getPort() != DEFAULT_PORT)
-                e.disallow(Result.KICK_OTHER, "\u00A77Please log in using \u00A7F" + desiredname);
+                e.disallow(Result.KICK_OTHER, ChatColor.GRAY +"Please log in using " + ChatColor.WHITE + names.get(0));
             else
-                e.disallow(Result.KICK_OTHER, "\u00A77Please log in using \u00A7F" + ip);
+                e.disallow(Result.KICK_OTHER, ChatColor.GRAY + "Please log in using " + ChatColor.WHITE + addresses.get(0));
             getLogger().info(e.getPlayer().getName() + " tried to log in using " + e.getHostname());
         }
     }
-    
+
     private void setupMetrics() {
         try {
             MetricsLite metrics = new MetricsLite(this);
