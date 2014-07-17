f9b82553a496ebf301c0ee53f5121b1b7aaaa4e7
Chris Darnell <chris@cedeel.com>
2014-07-17 18:35:12 -0400

    Changed package

--- src/main/java/com/cedeel/urienforcer/URIEnforcer.java
+++ src/main/java/com/cedeel/urienforcer/URIEnforcer.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package be.darnell.urienforcer;
+package com.cedeel.urienforcer;
 
 import java.io.IOException;
 import java.util.ArrayList;
